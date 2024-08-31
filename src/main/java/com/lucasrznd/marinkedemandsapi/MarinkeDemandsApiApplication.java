4b9209bebe523439d8bd82ec73f2d0b85152581c
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-31 10:22:31 -0300

    chore: create project structure

---
+++ src/main/java/com/lucasrznd/marinkedemandsapi/MarinkeDemandsApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucasrznd.marinkedemandsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarinkeDemandsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarinkeDemandsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasrznd/marinkedemandsapi/MarinkeDemandsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasrznd.marinkedemandsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarinkeDemandsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
