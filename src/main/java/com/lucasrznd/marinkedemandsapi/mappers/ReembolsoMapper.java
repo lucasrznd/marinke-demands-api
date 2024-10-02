79393dcb88604bec5bd134ea7f3a1037e8dd506f
Lucas Rezende <lucasffrezende@gmail.com>
2024-10-02 15:57:30 -0300

    feat: add refund mapper

---
+++ src/main/java/com/lucasrznd/marinkedemandsapi/mappers/ReembolsoMapper.java
@@ -0,0 +1,27 @@
+package com.lucasrznd.marinkedemandsapi.mappers;
+
+import com.lucasrznd.marinkedemandsapi.dtos.request.ReembolsoRequest;
+import com.lucasrznd.marinkedemandsapi.dtos.response.ReembolsoResponse;
+import com.lucasrznd.marinkedemandsapi.entities.Reembolso;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        uses = {DemandaMapper.class})
+public interface ReembolsoMapper {
+
+    @Mapping(target = "tipoReembolsoDescricao", source = "tipoReembolso.descricao")
+    @Mapping(target = "numeroContrato", source = "demanda.numeroContrato")
+    @Mapping(target = "nomeContratado", source = "contratado.nome")
+    ReembolsoResponse toResponse(final Reembolso reembolso);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "tipoReembolso.id", source = "tipoReembolsoId")
+    @Mapping(target = "demanda.idContratacao", source = "demandaId")
+    @Mapping(target = "contratado.id", source = "contratadoId")
+    Reembolso toEntity(final ReembolsoRequest request);
+
+    Reembolso update(final ReembolsoRequest request, @MappingTarget Reembolso reembolso);
+
+}
